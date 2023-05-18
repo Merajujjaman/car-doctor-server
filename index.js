1391f1d95cba0d0b03380aeaca6b7e353d8578e6
Merajujjaman <asmmerajujjaman@gmail.com>
2023-05-18 13:49:24 +0600

    jwt done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,13 +22,41 @@ const client = new MongoClient(uri, {
     }
 });
 
+//jwt verify:
+const varifyJwt =(req, res, next) =>{
+    const authorization = req.headers.authorization
+    // console.log(authorization);
+    if(!authorization){
+       return res.status(401).send({error: true, message: 'unauthorized token'})
+    }
+    const token = authorization.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, (error, decoded) => {
+        
+        if(error){
+            return res.status(401).send({error:true, message: 'unauthorized access'})
+        }
+
+        req.decoded = decoded;
+        next()
+    })
+}
+
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const serviceCollection = client.db('carDoctor').collection('services')
         const bookingCollection = client.db('carDoctor').collection('bookings')
+        
+        // jwt:
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN, {expiresIn: '1h' })
+            res.send({token})
+        })
 
+        //service routs
         app.get('/services', async(req, res) => {
             const cursor = serviceCollection.find()
             const result = await cursor.toArray()
@@ -36,7 +65,7 @@ async function run() {
 
         app.get('/services/:id', async(req, res) => {
             const id = req.params.id;
-            console.log(id);
+            
             const query = {_id: new ObjectId(id)}
             const options = {
                 projection: {  title: 1, service_id: 1, img: 1, price:1 },
@@ -54,8 +83,12 @@ async function run() {
             res.send(result)
         })
 
-        app.get('/bookings', async(req, res) => {
-            console.log(req.query.email);
+        app.get('/bookings', varifyJwt, async(req, res) => {
+            const decoded = req.decoded;
+            
+            if(decoded.email !== req.query.email){
+                return res.status(403).send({error: true, message: 'forbidden access'})
+            }
             let query = {}
             if(req?.query?.email){
                 query = {email: req.query.email}
